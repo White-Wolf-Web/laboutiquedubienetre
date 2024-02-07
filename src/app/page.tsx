5684debab5ed4afe3ac28c203b9586d05a751d93
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-02-07 23:24:14 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
+  <>
+  <main>
  <h1 className="text-center text-emerald-300"> la boutique du bien-être</h1>
-  
+ <p className="text-white">Encens, bougies, bol, </p>
+ </main>
+ </>
 }
