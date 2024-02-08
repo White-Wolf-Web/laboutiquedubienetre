7b73c56823a40daf3ad2bb27a27407f164e052d2
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-02-08 07:24:49 +0100

    tzilwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Home() {
-  <>
-  <main>
- <h1 className="text-center text-emerald-300"> la boutique du bien-être</h1>
- <p className="text-white">Encens, bougies, bol, </p>
- </main>
- </>
+  return (
+    <>
+      <Head>
+        <title>La Voie du Bien-Être - Massage Shiatsu</title>
+        <meta name="description" content="Profitez d'un moment de détente avec nos massages Shiatsu à [Votre Ville]. La Voie du Bien-Être est votre espace de relaxation et de soin pour le corps et l'esprit." />
+        <link rel="canonical" href="https://lavoiedubienetre.be" />
+        <script type="application/ld+json">
+    {`
+      {
+        "@context": "https://schema.org",
+        "@type": "HealthAndBeautyBusiness",
+        "name": "La Voie du Bien-Être",
+        "description": "Spécialiste en massage Shiatsu à [Votre Ville]. Offrez-vous un moment de détente et de bien-être.",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Votre adresse",
+          "addressLocality": "[Votre Ville]",
+          "postalCode": "[Votre Code Postal]",
+          "addressCountry": "FR"
+        },
+        "telephone": "[Votre Numéro de Téléphone]",
+        "image": "URL de l'image de votre entreprise",
+        "url": "https://lavoiedubienetre.be"
+      }
+    `}
+  </script>
+      </Head>
+      <main>
+        <h1>Bienvenue à La Voie du Bien-Être</h1>
+        <p>Découvrez un havre de paix où le massage Shiatsu ouvre la porte à une relaxation profonde et un bien-être renouvelé.</p>
+        <section>
+          <h2 className='font-bold underline'>Nos Services</h2>
+          <Image src="/path-to-your-image.jpg" alt="Massage Shiatsu" width={640} height={360} />
+          <p>Le Shiatsu, une thérapie manuelle japonaise, aide à rééquilibrer l énergie vitale pour une santé optimale.</p>
+        </section>
+        {/* Autres sections et contenu */}
+      </main>
+    </>
+  );
 }
