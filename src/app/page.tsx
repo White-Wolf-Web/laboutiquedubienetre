386178573d6c6726fc3e0f5c71513cb026ab6376
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-02-08 07:34:27 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>La Voie du Bien-Être - Massage Shiatsu</title>
-        <meta name="description" content="Profitez d'un moment de détente avec nos massages Shiatsu à [Votre Ville]. La Voie du Bien-Être est votre espace de relaxation et de soin pour le corps et l'esprit." />
-        <link rel="canonical" href="https://lavoiedubienetre.be" />
-        <script type="application/ld+json">
-    {`
-      {
-        "@context": "https://schema.org",
-        "@type": "HealthAndBeautyBusiness",
-        "name": "La Voie du Bien-Être",
-        "description": "Spécialiste en massage Shiatsu à [Votre Ville]. Offrez-vous un moment de détente et de bien-être.",
-        "address": {
-          "@type": "PostalAddress",
-          "streetAddress": "Votre adresse",
-          "addressLocality": "[Votre Ville]",
-          "postalCode": "[Votre Code Postal]",
-          "addressCountry": "FR"
-        },
-        "telephone": "[Votre Numéro de Téléphone]",
-        "image": "URL de l'image de votre entreprise",
-        "url": "https://lavoiedubienetre.be"
-      }
-    `}
-  </script>
+        <title>Boutique en ligne de produits bien-être</title>
+        <meta name="description" content="Encens bougies, pierres, Pendule, bol, huiles, huiles essentiels, lampe de sel, soin du corps ..." />
+        <link rel="canonical" href="https://www.laboutiquedubienetre.net/" />
+       
       </Head>
       <main>
-        <h1>Bienvenue à La Voie du Bien-Être</h1>
-        <p>Découvrez un havre de paix où le massage Shiatsu ouvre la porte à une relaxation profonde et un bien-être renouvelé.</p>
+        <h1>Bienvenue à la boutique du Bien-Être</h1>
+        <p>Découvrez nos produits un bien-être renouvelé.</p>
         <section>
           <h2 className='font-bold underline'>Nos Services</h2>
           <Image src="/path-to-your-image.jpg" alt="Massage Shiatsu" width={640} height={360} />
-          <p>Le Shiatsu, une thérapie manuelle japonaise, aide à rééquilibrer l énergie vitale pour une santé optimale.</p>
+         
+          <p>les encens</p> <p>les encens</p> <p>les bougies</p> <p>les pendules</p> <p>les pierres</p>
+          <p>les bols tibétains</p> <p>les huiles essentiels</p>
         </section>
-        {/* Autres sections et contenu */}
+
       </main>
     </>
   );
